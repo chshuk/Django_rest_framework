ace5f5c2e7f722a216a31c25bbf3fb626bd5452f
Alexandra Timoshchuk <chshuk@mail.ru>
2023-01-08 00:44:59 +0300

    Lesson_02 corsheaders

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/authors')
+        axios.get('http://127.0.0.1:8000/api/authors/')
             .then(response => {
                 const authors = response.data
                     this.setState(
