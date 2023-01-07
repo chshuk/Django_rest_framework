e1708fdd4a0f7053bd24cdc1d48441c7182d0411
Alexandra Timoshchuk <chshuk@mail.ru>
2023-01-07 23:52:11 +0300

    Lesson_2 corsheaders

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import AuthorList from './components/Author.js';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'authors': []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/authors')
+            .then(response => {
+                const authors = response.data
+                    this.setState(
+                        {
+                            'authors': authors
+                        }
+                    )
+            }).catch(error => console.log(error))
+    }
+
+
+    render () {
+        return (
+            <div>
+                <AuthorList authors={this.state.authors} />
+            </div>
+        )
+    }
 }
 
 export default App;
+
---
+++ library/frontend/src/components/Author.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+const AuthorItem = ({author}) => {
+    return(
+        <tr>
+            <td>
+                {author.first_name}
+            </td>
+            <td>
+                {author.last_name}
+            </td>
+            <td>
+                {author.birth_year}
+            </td>
+        </tr>
+    )
+}
+
+const AuthorList = ({authors}) => {
+    return (
+        <table>
+            <th>
+                First name
+            </th>
+            <th>
+                Last Name
+            </th>
+            <th>
+                Birth year
+            </th>
+            {authors.map((author) => <AuthorItem author={author} />)}
+        </table>
+    )
+}
+
+export default AuthorList
