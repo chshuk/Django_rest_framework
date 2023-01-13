8113b619c3c4b237b6406257556e92dbffa67aec
Alexandra Timoshchuk <chshuk@mail.ru>
2023-01-13 17:27:10 +0300

    Lesson_3 created serializes

--- library/applic/serializers.py
+++ library/applic/serializers.py
@@ -1,8 +1,36 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
+from rest_framework.serializers import HyperlinkedModelSerializer, StringRelatedField
+from .models import Author, Book, Biography, Article
 
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Author
+        fields = '__all__'
+
+
+class AuthorSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+class BiographySerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Biography
+        fields = ['text', 'author']
+
+
+class ArticleSerializer(HyperlinkedModelSerializer):
+    author = AuthorSerializer()
+
+    class Meta:
+        model = Article
+        exclude = ['id']
+
+
+class BookSerializer(HyperlinkedModelSerializer):
+    authors = StringRelatedField(many=True)
+
+    class Meta:
+        model = Book
         fields = '__all__'
