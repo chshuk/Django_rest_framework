df0047f64836065d9e8403470b8bc78567961a87
Alexandra Timoshchuk <chshuk@mail.ru>
2023-01-19 13:09:14 +0300

    Lesson_05 HashRouter

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import AuthorList from './components/Author.js';
+import BookList from './components/Books.js'
+import {HashRouter, Route} from 'react-router-dom'
 // import axios from 'axios';
 
 
@@ -12,8 +14,15 @@ class App extends React.Component {
         const author1 = {id: 1, first_name: 'Грин', birth_year: 1880}
         const author2 = {id: 2, first_name: 'Пушкин', birth_year: 1799}
         const authors = [author1, author2]
+        const book1 = {id: 1, name: 'Алые паруса', author: author1}
+        const book2 = {id: 2, name: 'Золотая цепь', author: author1}
+        const book3 = {id: 3, name: 'Пиковая дама', author: author2}
+        const book4 = {id: 4, name: 'Руслан и Людмила', author: author2}
+        const books = [book1, book2, book3, book4]
+
         this.state = {
-            'authors': authors
+            'authors': authors,
+            'books': books
         }
 
 
@@ -35,7 +44,11 @@ class App extends React.Component {
     render () {
         return (
            <div className="App">
-                <AuthorList items={this.state.authors} />
+            <HashRouter>
+                <Route exact path='/' component={() => <AuthorList items={this.state.authors} />} />
+                <Route exact path='/books' component={() => <BookList items={this.state.books} />} />
+
+            </HashRouter>
            </div>
 
         )
