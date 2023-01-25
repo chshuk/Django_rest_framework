7a6b4bd989e0acd02e0c6c638b7bf482242417de
Alexandra Timoshchuk <chshuk@mail.ru>
2023-01-25 16:59:07 +0300

    Lesson_6 auth-token

--- library/applic/views.py
+++ library/applic/views.py
@@ -5,9 +5,9 @@ from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django_filters import rest_framework as filters
-from .models import Author, Biography, Book, Article
-from .serializers import AuthorSerializer, BiographySerializer, BookSerializer, ArticleSerializer, ArticleAPISerializer, \
-    ArticleSetSerializer
+from .models import Author, Biography, Book
+from .serializers import AuthorSerializer, BiographySerializer, BookSerializer
+
 
 
 class AuthorViewSet(ModelViewSet):
@@ -24,51 +24,3 @@ class BiographyViewSet(ModelViewSet):
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-
-class ArticleViewSet(ViewSet):
-
-    @action(detail=True, methods=['get'])
-    def article_name_only(self, request, pk=None):
-        article = get_object_or_404(Article, pk=pk)
-        return Response({'article.name': article.name})
-
-    def list(self, request):
-        articles = Article.objects.all()
-        serializer = ArticleSetSerializer(articles, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        article = get_object_or_404(Article, pk=pk)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
-
-
-class ArticleAPIView(APIView):
-    def get(self, request, format=None):
-        articles = Article.objects.all()
-        serializer = ArticleAPISerializer(articles, many=True)
-        return Response(serializer.data)
-
-
-class ArticleListAPIView(ListAPIView):
-    # renderer_classes = [JSONRenderer]
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleQuerysetFilterViewSet(ModelViewSet):
-    serializer_class = ArticleSetSerializer
-    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
-    queryset = Article.objects.all()
-
-    def get_queryset(self):
-        return Article.objects.filter(name__contains='Пушкин')
-
-
-class ArticleDjangoFilterViewSet(ModelViewSet):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSetSerializer
-    filterset_fields = ['name', 'author']
-
-
