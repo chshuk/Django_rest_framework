5427f11ba41a9c56ffdda0f7093bd0c0d092f618
Alexandra Timoshchuk <chshuk@mail.ru>
2023-02-20 16:53:55 +0300

    Lesson 12 wait postgress

--- library/library/settings.py
+++ library/library/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library',
+        'USER': 'admin',
+        'PASSWORD': '1111',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
